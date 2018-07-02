c71c49d25658ebd2368e40a89a415688ca56b35f
psanchos <prg1alex@gmail.com>
2018-07-02 18:54:54 +0300

    Server socket

--- sockets/src/main/java/ru/sbrf/jschool/socket/tcp/ServerSocketExample.java
+++ sockets/src/main/java/ru/sbrf/jschool/socket/tcp/ServerSocketExample.java
@@ -1,8 +1,12 @@
 package ru.sbrf.jschool.socket.tcp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerSocketExample {
 
@@ -13,11 +17,10 @@ public class ServerSocketExample {
         ServerSocket serverSocket = new ServerSocket(PORT);
         System.out.println(String.format("Application started, and listening port: %d", PORT));
         System.out.println("Wait connect...");
-
-        try (
-                Socket socket = serverSocket.accept();
-        ) {
-            System.out.println(String.format("connect accepted, client ip %s", socket.getInetAddress()));
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+        while (true) {
+           pool.execute(new SocketThread(serverSocket.accept()));
         }
+
     }
 }
