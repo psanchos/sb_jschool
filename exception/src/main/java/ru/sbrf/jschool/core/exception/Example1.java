c1808045a0f0fba7015f937ce26fbf777a0bbc98
psanchos <prg1alex@gmail.com>
2019-11-14 17:50:15 +0300

    Примеры по лекции exceptions

---
+++ exception/src/main/java/ru/sbrf/jschool/core/exception/Example1.java
@@ -0,0 +1,19 @@
+package ru.sbrf.jschool.core.exception;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        try{
+            throw new Exception1();
+        }catch (Exception1 ex){
+            System.out.println("Handle exception 1");
+            throw new  Exception2();
+        }catch (Exception2 ex){
+            throw new Exception3();
+        }
+    }
+
+    static class Exception1 extends RuntimeException{}
+    static class Exception2 extends RuntimeException{}
+    static class Exception3 extends RuntimeException{}
+}
